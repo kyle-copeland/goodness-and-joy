f8b2b700a10632f2e7a65cc6a1466cf498dc2b04
Kyle Copeland <copelandky@gmail.com>
2020-12-21 20:08:07 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: "Goodness & Joy",
+  },
+  plugins: [
+    "gatsby-plugin-offline",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
